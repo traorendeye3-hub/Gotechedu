758c029f990e1f0cd2c4f07f2a2414f617dc0df2
khadidiatougit config --global user.email <votre-email@example.com>
2026-08-09 22:11:00 +0000

    Fix navbar styleUrl path

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   // Importation explicite du chemin
