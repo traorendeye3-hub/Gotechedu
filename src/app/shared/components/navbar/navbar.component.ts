15d2e9369020363f7b89f83e267514258be454a7
khadidiatougit config --global user.email <votre-email@example.com>
2026-08-09 21:52:19 +0000

    Fix absolute path for logo

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -6,6 +6,9 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  // Importation explicite du chemin
+  logoPath = 'images/logo.jpeg'; 
+}
